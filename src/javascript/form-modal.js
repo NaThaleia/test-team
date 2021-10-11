b28659803bbd6cc9f343c4f2b3077476422ec219
Tanya Kuchma <kuchma.tanya@ukr.net>
2021-10-12 01:25:52 +0300

    fix-modal-form-from-mobile

--- src/javascript/form-modal.js
+++ src/javascript/form-modal.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
     openFormModalBtn: document.querySelector('[data-form-modal-open]'),
+    openFormModalBtn2: document.querySelector('[data-form-modal2-open]'),
     closeFormModalBtn: document.querySelector('[data-form-modal-close]'),
     modalForm: document.querySelector('[data-form-modal]'),
   };
 
   refs.openFormModalBtn.addEventListener('click', toggleModal);
+  refs.openFormModalBtn2.addEventListener('click', toggleModal);
   refs.closeFormModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
