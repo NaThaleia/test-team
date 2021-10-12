424f636799f7d21c2d41f311fd66bffc0feb6405
NaThaleia <miss.mekh@gmail.com>
2021-10-12 22:49:12 +0300

    fix script

--- src/javascript/mobile-menu.js
+++ src/javascript/mobile-menu.js
@@ -3,10 +3,12 @@
     openMenuBtn: document.querySelector('.js-open-menu'),
     closeMenuBtn: document.querySelector('.js-close-menu'),
     mobileMenu: document.querySelector('.js-menu-container'),
+    mobileMenuLinks: document.querySelectorAll('.mobile-menu__link'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.mobileMenuLinks.forEach(link => link.addEventListener('click', toggleMenu));
 
   function toggleMenu() {
     refs.mobileMenu.classList.toggle('is-hidden');
